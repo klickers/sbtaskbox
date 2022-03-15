3a6b5fe74a6639edb316e0869977e6ae6f2415b4
klickers <bluebirid228@gmail.com>
2022-03-14 17:25:59 -0700

    complete Task component, preview, test

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.stories.mdx", 
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
